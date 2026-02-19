e7ad2386e5cee07b14acc041cf1578a091b725c6
Aasif Mohd <122384084+Mohdaasif97@users.noreply.github.com>
2026-02-19 21:00:15 +0100

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://salcal.vercel.app',
+      url: 'https://www.minijob-netto-rechner.app',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1.0,
