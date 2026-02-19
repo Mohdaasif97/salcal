1973f34525fe0a3ee7fd09c7e93d0daed1599756
Aasif Mohd <122384084+Mohdaasif97@users.noreply.github.com>
2026-02-19 21:23:10 +0100

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://www.minijob-netto-rechner.app',
+      url: 'https://www.minijob-netto-rechner.de',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1.0,
